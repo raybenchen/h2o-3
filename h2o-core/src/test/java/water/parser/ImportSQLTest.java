d02cfe402fa98dffb1df5922c3a1456b707aae1d
ludi <ludi@h2o.ai>
2016-04-12 14:08:38 -0700

    HEXDEV-464 Use class name to add val to chunk, enable tests

---
+++ h2o-core/src/test/java/water/parser/ImportSQLTest.java
@@ -0,0 +1,48 @@
+package water.parser;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import water.TestUtil;
+import water.fvec.Frame;
+import water.jdbc.SQLManager;
+
+import static org.junit.Assert.assertTrue;
+
+public class ImportSQLTest extends TestUtil{
+  private String conUrl = "jdbc:mysql://172.16.2.178:3306/ingestSQL?&useSSL=false";
+  String user = "root";
+  String password = "0xdata";
+  boolean optimize = true;
+  
+  @BeforeClass
+  static public void setup() {stall_till_cloudsize(1);}
+
+  @Test
+  public void citibike20k() {
+    String table = "citibike20k";
+    Frame sql_f = SQLManager.importSqlTable(conUrl, table, user, password, optimize).get();
+    assertTrue(sql_f.numRows() == 2e4);
+    assertTrue(sql_f.numCols() == 15);
+    sql_f.delete();
+  }
+  
+  @Test
+  public void allSQLTypes() {
+    String table = "allSQLTypes";
+    Frame sql_f = SQLManager.importSqlTable(conUrl, table, user, password, optimize).get();
+    sql_f.delete();
+    
+  }
+  
+  @Ignore @Test
+  public void airlines() {
+    String conUrl = "jdbc:mysql://localhost:3306/menagerie?&useSSL=false";
+    String table = "air";
+    String password = "ludi";
+    Frame sql_f = SQLManager.importSqlTable(conUrl, table, user, password, optimize).get();
+    sql_f.delete();
+  }
+  
+
+}
